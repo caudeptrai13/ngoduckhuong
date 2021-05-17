fb4e3e247862382ab9273938c05c1bd41ca61fe1
caudeptrai13 <58966374+caudeptrai13@users.noreply.github.com>
2021-05-17 20:54:02 +0700

    Bai tap 17/5/2021

--- Bai1String.py
+++ Bai1String.py
@@ -2,10 +2,10 @@ while True:
     try:
         print("Nhap ho va ten: ",end="")
         ten = input()
-        print(ten)
         print("Nhap tuoi: ",end="")
         tuoi = int(input())
         break
     except:
         pass
-print(ten,": " ,tuoi,"tuoi")
+print("""Ho va ten la : {0}
+Tuoi la : {1}""".format(ten.strip().title(),tuoi))
